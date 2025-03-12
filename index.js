a195af0c1460074ece765167e2e1280b7492e1d5
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-03-12 13:12:12 +0300

    array-methods

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function destructivelyAppendCat(Ralph){
+    cats.push("Ralph")
+  }
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(Broom) {
+    return [...cats, "Broom"]; 
+}
+function prependCat(Arnold) {
+    return['Arnold'].concat(cats);
+}
+function removeFirstCat(){
+     return cats.slice(1);
+}
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
